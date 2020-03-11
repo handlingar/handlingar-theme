c5b52ef6317a18c569e88ad023b37c8037cc60c8
Ellewe <ellelingh@hotmail.se>
2020-03-11 15:37:22 +0100

    Update custom-routes.rb

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -12,7 +12,7 @@ end
 # Here you can override or add to the pages in the core website
 
 Rails.application.routes.draw do
-  get '/help/för_ideella' => 'help#för_ideella'
+  get '/help/for_nonprofits' => 'help#for_nonprofits'
 end
 
 
