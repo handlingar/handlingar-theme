0f46256eac87338d67dc6dfb82191c5d6d168f98
Ellewe <ellelingh@hotmail.se>
2020-03-11 15:26:50 +0100

    Update custom-routes.rb För Ideella

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -7,3 +7,12 @@ Rails.application.routes.draw do
   # Additional help page example
   # get '/help/help_out' => 'help#help_out'
 end
+
+-*- encoding : utf-8 -*-
+# Here you can override or add to the pages in the core website
+
+Rails.application.routes.draw do
+  get '/help/för_ideella' => 'help#för_ideella'
+end
+
+
