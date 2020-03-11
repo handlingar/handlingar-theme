a23f9c73b5375112c40d97640c1cc06bd3527459
Ellewe <ellelingh@hotmail.se>
2020-03-11 16:23:25 +0100

    Update custom-routes.rb

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   # get '/help/help_out' => 'help#help_out'
 end
 
--*- encoding : utf-8 -*-
+#-*- encoding : utf-8 -*-
 # Here you can override or add to the pages in the core website
 
-Rails.application.routes.draw do
-  get '/help/nonprofits' => 'help#nonprofits'
-end
+#Rails.application.routes.draw do
+ # get '/help/nonprofits' => 'help#nonprofits'
+#end
 
 
